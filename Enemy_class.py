9437ea5e8f4b4a2766c86d8363e199170a49c0fb
danhGIT3020 <dtran44@students.kennesaw.edu>
2026-04-26 18:51:07 -0400

    Update Enemy_class.py

--- Enemy_class.py
+++ Enemy_class.py
@@ -1,5 +1,18 @@
-import random as randint
 class Enemy:
-  def __init__(self, decision, health):
-    self.decision = 0
-    self.health = 0
+  def __init__(self, decision):
+    self.decision = decision
+    self.loaded = True
+    self.bowsprit = True
+    self.health = 100
+
+def enemy_turn(self):
+  if self.decision == 1:
+    print("The enemy ship has fired their cannons!")
+    self.loaded = False
+  if self.decision == 2:
+    print("The enemy ship has reloaded their cannons.")
+    self.loaded = True
+  if self.decision == 3 and self.bowsprit == True:
+    print("The enemy has evaded your attack!")
+  if self.decision == 4 and self.bowsprit == True:
+    print("The enemy charged their ship at you!")
