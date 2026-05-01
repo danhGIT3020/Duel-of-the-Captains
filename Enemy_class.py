256087167d403d2851fe3c8857f1eeae07e4c76a
danhGIT3020 <dtran44@students.kennesaw.edu>
2026-05-01 09:58:41 -0400

    Update Enemy_class.py

--- Enemy_class.py
+++ Enemy_class.py
@@ -5,14 +5,14 @@ class Enemy:
     self.bowsprit = True
     self.health = 100
 
-def enemy_turn(self):
-  if self.decision == 1:
-    print("The enemy ship has fired their cannons!")
-    self.loaded = False
-  if self.decision == 2:
-    print("The enemy ship has reloaded their cannons.")
-    self.loaded = True
-  if self.decision == 3 and self.bowsprit == True:
-    print("The enemy has evaded your attack!")
-  if self.decision == 4 and self.bowsprit == True:
-    print("The enemy charged their ship at you!")
+  def enemy_turn(self):
+    if self.decision == 1 and self.loaded == True:
+      print("The enemy ship has fired their cannons!")
+      self.loaded = False
+    elif self.decision == 2:
+      print("The enemy ship has reloaded their cannons.")
+      self.loaded = True
+    if self.decision == 3 and self.bowsprit == True:
+      print("The enemy has evaded your attack!")
+    if self.decision == 4 and self.bowsprit == True:
+      print("The enemy charged their ship at you!")
